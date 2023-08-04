c39df9d0f5699a6823b246ee6d362a60cac12980
Vitoria Cangussu <130415445+Vitoriagc@users.noreply.github.com>
2023-08-04 19:15:49 -0300

    Add files via upload

---
+++ aluno/listaalunos.php
@@ -0,0 +1,137 @@
+<?php 
+  require_once('../conexao.php');
+  $retorno = $conexao->prepare('SELECT * FROM aluno');
+  $retorno->execute();
+
+?>       
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="princ">
+</body>
+</html>
+<table> 
+<thead>
+<tr>
+<th>ID</th>
+<th>NOME</th>
+<th>IDADE</th>
+<th>MATRICULA</th>
+<th>CPF</th>
+<th>DATA DE NASCIMENTO</th>
+                    <th>ENDEREÇO</th>
+                    <th>ESTATUS</th>
+</tr>
+
+  </thead>
+ <tbody>
+<tr>
+<?php foreach($retorno->fetchall() as $value) { ?>
+<tr>
+<td> <?php echo $value['id'] ?>   </td> 
+<td> <?php echo $value['nome']?>  </td> 
+<td> <?php echo $value['idade']?> </td> 
+<td> <?php echo $value['matricula']?> </td> 
+<td> <?php echo $value['cpf']?> </td> 
+<td> <?php echo $value['dtnasc']?> </td> 
+                            <td> <?php echo $value['endereco']?> </td>  
+                            <td> <?php echo $value['estatus']?> </td>
+
+    <td>
+        <form method="POST" action="altaluno.php">
+        <input name="id" type="hidden" value="<?php echo $value['id'];?>"/>
+        <button name="alterar"  type="submit">Alterar</button>
+    </form>
+
+                             </td> 
+
+                             <td>
+                               <form method="GET" action="crudaluno.php">
+                                        <input name="id" type="hidden" value="<?php echo $value['id'];?>"/>
+                                        <button name="excluir"  type="submit">Excluir</button>
+                                </form>
+
+                             </td> 
+
+
+                       
+                      </tr>
+                    <?php  }  ?> 
+                 </tr>
+            </tbody>
+        </table>
+<?php         
+   echo "<button class='button button3'><a href='http://localhost/vitoria/cadastroluno'>Voltar</a></button>";
+?>
+</div>
+<style>
+    body {
+    height: auto;
+    width: auto;
+}
+
+.princ {
+    background-color: rgb(191, 186, 231);
+    width: 183vh;
+    min-height: 97vh;
+    height: auto;
+    margin-left: 23px;
+    box-shadow: 0px 0px 15px 5px rgb(56, 56, 56);
+    text-align: center;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    font-size: 2ch;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+h2{
+    margin: 0;
+    margin-bottom: 7vh
+}
+
+table {
+    height: 60%;
+    width: 80%;
+    border-collapse: collapse;
+}
+
+tr{
+    text-align: center;
+}
+th, td{
+    border: 3px solid black;
+}
+
+button {
+    background-color: rgb(78, 243, 128);;
+    height: 4vh;
+    width: 15vh;
+    border-radius: 10px;
+    border: 1px black solid;
+   
+    margin: 1vh;
+    cursor: pointer;
+    
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+
+button:hover {
+    background-color: rgb(43, 158, 78);
+}
+
+a:link {
+    text-decoration: none;
+}
+
+a {
+    color: black;
+}
+</style>
